28101cf587d8311836f7e156b72c9510d3586615
pborel <phil_borel@yahoo.com>
2017-06-13 12:56:53 -0400

    fix description for import control

--- lib/active_shipping/carriers/ups.rb
+++ lib/active_shipping/carriers/ups.rb
@@ -596,7 +596,7 @@ module ActiveShipping
 
           packages.each do |package|
             xml.Product do |xml|
-              xml.Description(package.options[:description])
+              xml.Description(package.options[:description] || options[:description])
               xml.CommodityCode(package.options[:commodity_code])
               xml.OriginCountryCode(options.country_of_origin || origin.country_code(:alpha2))
               xml.Unit do |xml|
