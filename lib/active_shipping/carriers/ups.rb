697ad7e6e5ab0d9ccda28bf2bf6addb9baa498bc
brian31988 <brian31988@gmail.com>
2019-06-12 13:56:07 -0400

    test products

--- lib/active_shipping/carriers/ups.rb
+++ lib/active_shipping/carriers/ups.rb
@@ -614,7 +614,7 @@ module ActiveShipping
             xml.TermsOfShipment(options[:terms_of_shipment])
           end
 
-          packages.first.options[:products] do |package|
+          packages.first.options[:products].each do |package|
             xml.Product do |xml|
               xml.Description(package.options[:description] || options[:description])
               xml.CommodityCode(package.options[:commodity_code])
